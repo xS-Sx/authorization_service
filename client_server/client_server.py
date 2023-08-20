7079c4e6a87125401660c9c65a314d37da1d53f2
xS-Sx <tinchy.l.x@gmail.com>
2023-08-21 01:47:42 +0200

    fix: fix client_server

--- client_server/client_server.py
+++ client_server/client_server.py
@@ -6,7 +6,7 @@ import requests
 
 AUTH_ENDPOINT = "http://localhost:8001/authorize"
 TOKEN_ENDPOINT = "http://localhost:8001/token"
-RES_ENDPOINT = "http://localhost:8002/users"
+RES_ENDPOINT = "http://localhost:8002/example_resource"
 REDIRECT_URL = "http://localhost:8000/auth_cb"
 
 
@@ -36,7 +36,7 @@ async def get_authorization_token():
         </div>
         <div>
             <p>
-                <a href="{auth_endpoint}?response_type=code&client_id={client_id}&redirect_url=  {redirect_url}">click here
+                <a href="{auth_endpoint}?response_type=code&client_id={client_id}&redirect_url={redirect_url}">click here
                 </a>
                 to sign in via authorization server to access api resource
             </p>
@@ -68,24 +68,30 @@ async def get_access_token(code: str | None = None):
     global example_api_access_token
 
     if code is not None:
-        resp = requests.post(    
-            TOKEN_ENDPOINT,
-            data = {
-                "code": code,
-                "grant_type": "code",
-                "redirect_url": REDIRECT_URL,
-                "client_id": CLIENT_ID,
-                "client_secret": CLIENT_SECRET
-            }
-        )
+        try:
+            resp = requests.post(    
+                TOKEN_ENDPOINT,
+                data = {
+                    "code": code,
+                    "grant_type": "code",
+                    "redirect_url": REDIRECT_URL,
+                    "client_id": CLIENT_ID,
+                    "client_secret": CLIENT_SECRET
+                }
+            )
 
-        if resp.status_code == 200:
-            example_api_access_token = resp.json().get("access_token")
-            return RedirectResponse(app.url_path_for("read_api_resource"))
-        else:
+            if resp.status_code == 200:
+                example_api_access_token = resp.json().get("access_token")
+                return RedirectResponse(app.url_path_for("read_api_resource"))
+            else:
+                raise HTTPException(
+                    status_code=500,
+                    detail="Fail to acquire access from auth_server"
+                )
+        except:
             raise HTTPException(
-                status_code=resp.status_code,
-                detail="Fail to be authorized by authorization server to access api resource"
+                status_code=500,
+                detail="Fail to get response from auth_server"
             )
 
 
@@ -95,18 +101,25 @@ async def read_api_resource():
     Return api resource from api_server if client is authorized with access token.
     If client is not authorzied, user is redirect to sign in to acquire access token for client.
     """
-    global example_api_access_token
 
     if example_api_access_token is not None:
-        resp = requests.get(RES_ENDPOINT, headers = {
-            "Authorization": f"Bearer {example_api_access_token}"
-        })
-        if resp.status_code == 200:
-            return resp.json()
-        else:
+        try:
+            resp = requests.get(RES_ENDPOINT, headers = {
+                "Authorization": f"Bearer {example_api_access_token}"
+            })
+            if resp.status_code == 200:
+                return resp.json()
+            else:
+                raise HTTPException(
+                    status_code=resp.status_code,
+                    detail="Fail to retrive api resource"
+                )
+        except:
             raise HTTPException(
-                status_code=resp.status_code,
-                detail="Fail to retrive api resource"
+                status_code=500,
+                detail=(
+                    "Fail to retrive api resource. " "api_server may be down"
+                )
             )
     else:
         return RedirectResponse(app.url_path_for("get_authorization_token"))
